8f25ae3022e7ea5c63e44eccfd5470bdf3709c7a
John Clark <inindev@gmail.com>
2017-04-08 14:23:08 -0400

    path handling improvement
    Signed-off-by: John Clark <inindev@gmail.com>

--- hlk-rm04/tools/scripts/http_file_server.py
+++ hlk-rm04/tools/scripts/http_file_server.py
@@ -21,84 +21,85 @@ from __future__ import print_function
 import os, re, sys
 import socket, SocketServer
 
+MYDIR = os.path.dirname(os.path.abspath(__file__))
+IN_FILEPATH = os.path.realpath(os.path.join(MYDIR, '../../bin/lede-17.01-ramips-rt305x-hlk-rm04-squashfs-sysupgrade.bin'))
+OUT_FILE = sysupgrade.bin
 PORT = 8080
-OUT_FILE = "sysupgrade.bin"
-IN_FILEPATH = os.path.dirname(os.path.realpath(__file__)) + "/../../bin/lede-17.01-ramips-rt305x-hlk-rm04-squashfs-sysupgrade.bin"
 
 
 class HttpRequestHandler(SocketServer.BaseRequestHandler):
     def setup(self):
-        print("client connect: \033[32m{}\033[0m".format(self.request.getpeername()[0]));
+        print('client connect: \033[32m{}\033[0m'.format(self.request.getpeername()[0]));
 
     def handle(self):
         data = self.request.recv(1024)
-        match = re.search("^GET /%s HTTP" % OUT_FILE, data)
+        match = re.search('^GET /%s HTTP' % OUT_FILE, data)
         if(match == None):
-            response  = "HTTP/1.1 404 Not Found\n"
-            response += "content-length: 14\n"
-            response += "content-type: text/plain\n"
-            response += "\n"
-            response += "404 Not Found\n"
+            response  = 'HTTP/1.1 404 Not Found\n'
+            response += 'content-length: 14\n'
+            response += 'content-type: text/plain\n'
+            response += '\n'
+            response += '404 Not Found\n'
             self.request.send(response)
             return
 
         filename = os.path.basename(IN_FILEPATH)
-        print("streaming file: \033[32m{}\033[0m to client: \033[32m{}\033[0m".format(filename, self.request.getpeername()[0]))
-        response  = "HTTP/1.1 200 OK\n"
-        response += "content-disposition: attachment; filename=%s\n" % OUT_FILE
-        response += "content-length: %d\n" % os.path.getsize(IN_FILEPATH)
-        response += "content-type: application/octet-stream\n"
-        response += "\n"
+        print('streaming file: \033[32m{}\033[0m to client: \033[32m{}\033[0m'.format(filename, self.request.getpeername()[0]))
+        response  = 'HTTP/1.1 200 OK\n'
+        response += 'content-disposition: attachment; filename=%s\n' % OUT_FILE
+        response += 'content-length: %d\n' % os.path.getsize(IN_FILEPATH)
+        response += 'content-type: application/octet-stream\n'
+        response += '\n'
         self.request.send(response)
         # body
-        sys.stdout.write("progress [")
-        with open(IN_FILEPATH, "rb") as f:
+        sys.stdout.write('progress [')
+        with open(IN_FILEPATH, 'rb') as f:
             while True:
                 chunk = f.read(32768)
                 if chunk:
-                    sys.stdout.write(".")
+                    sys.stdout.write('.')
                     self.request.send(chunk)
                 else:
-                    sys.stdout.write("]\n")
+                    sys.stdout.write(']\n')
                     break
 
     def finish(self):
-        print("client disconnect: \033[32m{}\033[0m".format(self.request.getpeername()[0]));
+        print('client disconnect: \033[32m{}\033[0m'.format(self.request.getpeername()[0]));
         return SocketServer.BaseRequestHandler.finish(self)
 
 
 def main():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
+        s.connect(('8.8.8.8', 80))
         myip = s.getsockname()[0]
     except:
-        print("error: failed to get local ip address")
+        print('error: failed to get local ip address')
         return(1)
 
-    httpd = SocketServer.TCPServer(("", PORT), HttpRequestHandler)
+    httpd = SocketServer.TCPServer(('', PORT), HttpRequestHandler)
 
-    print("\nwaiting for connection...")
-    print("  address: {}".format(myip))
-    print("  port: {}\n".format(PORT))
+    print('\nwaiting for connection...')
+    print('  address: {}'.format(myip))
+    print('  port: {}\n'.format(PORT))
 
-    print("to upgrade the a140808 device, run the following command:")
-    print("  \033[33mecho 3 > /proc/sys/vm/drop_caches && /sbin/sysupgrade -v http://{}{}/{}\033[0m\n".format(myip, "" if(PORT==80) else ":"+str(PORT), OUT_FILE));
+    print('to upgrade the a140808 device, run the following command:')
+    print('  \033[33mecho 3 > /proc/sys/vm/drop_caches && /sbin/sysupgrade -v http://{}{}/{}\033[0m\n'.format(myip, '' if(PORT==80) else ':'+str(PORT), OUT_FILE));
 
-    print("to factory reset the a140808 device, run the following command:")
-    print("  mtd -r erase rootfs_data\n")
+    print('to factory reset the a140808 device, run the following command:')
+    print('  mtd -r erase rootfs_data\n')
 
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print("\nexiting...")
+        print('\nexiting...')
         pass
     finally:
         httpd.server_close()
 
-    print("\nhttp server closed\n")
+    print('\nhttp server closed\n')
     return(0)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     exit(main())
